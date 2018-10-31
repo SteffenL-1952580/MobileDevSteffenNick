3547878975e9a2dbb74919c9b50be9dc990de8b6
steffenlen <steffen.lenaerts@student.pxl.be>
2018-10-31 18:50:12 +0100

    Adde back button in detailview

--- app/src/main/java/com/example/a11601743/firstproject/DetailActivity.java
+++ app/src/main/java/com/example/a11601743/firstproject/DetailActivity.java
@@ -2,16 +2,28 @@ package com.example.a11601743.firstproject;
 
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
-public class DetailActivity extends FragmentActivity { //AppCompatActivity
+public class DetailActivity extends AppCompatActivity { //FragmentActivity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        //my_child_toolbar is defined in the layout file
+        Toolbar myChildToolbar =(Toolbar) findViewById(R.id.detail_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
 
         Intent intent = getIntent();
 
